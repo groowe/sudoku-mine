14db04dfbe0d6df2a2478130eccd659457cad3f7
groowe <zivec.jiri@seznam.cz>
2019-08-02 12:14:13 +0200

    fresh try (3)

---
+++ sudoku3.py
@@ -0,0 +1,220 @@
+def newtry():
+    grid = [['0' for i in range(9)] for i in range(9)]
+    grid[0] = [3,0,0,2,0,0,1,0,0]
+    grid[1] = [0,0,5,7,1,3,0,0,0]
+    grid[2] = [0,0,7,4,6,0,8,0,0]
+    grid[3] = [8,0,0,3,0,0,0,1,2]
+    grid[4] = [7,0,0,8,5,0,0,0,9]
+    grid[5] = [9,2,0,0,4,6,0,0,0]
+    grid[6] = [0,7,0,0,0,0,0,4,6]
+    grid[7] = [0,1,0,0,0,0,2,5,8]
+    grid[8] = [0,0,0,5,0,4,0,9,0]
+    for i in grid:
+#        print(i)
+        for i in range(len(grid)):
+            for s in range(len(grid[i])):
+                if grid[i][s] == 0:
+                    grid[i][s] = '0'
+#    for i in grid:
+#        print(i)
+#    print("hardtry")
+    return grid
+
+def newtry2():
+    grid = [['0' for i in range(9)] for i in range(9)]
+
+    grid[0] = [0,0,0,0,5,0,4,0,0]
+    grid[1] = [8,0,0,0,0,3,5,0,0]
+    grid[2] = [0,0,0,0,0,2,8,9,0]
+    grid[3] = [6,0,5,0,0,1,0,0,0]
+    grid[4] = [0,1,0,4,0,9,0,3,0]
+    grid[5] = [0,0,0,6,0,0,1,0,8]
+    grid[6] = [0,8,6,1,0,0,0,0,0]
+    grid[7] = [0,0,2,3,0,0,0,0,9]
+    grid[8] = [0,0,4,0,9,0,0,0,0]
+    for i in grid:
+#        print(i)
+        for i in range(len(grid)):
+            for s in range(len(grid[i])):
+                if grid[i][s] == 0:
+                    grid[i][s] = '123456789'
+    return grid
+
+def newtry3():
+    grid = [['0' for i in range(9)] for i in range(9)]
+
+    grid[0] = [0,0,0,0,5,0,4,0,0]
+    grid[1] = [8,0,0,0,0,3,5,0,0]
+    grid[2] = [0,0,0,0,0,2,8,9,0]
+    grid[3] = [6,0,5,0,0,1,0,0,0]
+    grid[4] = [0,1,0,4,0,9,0,3,0]
+    grid[5] = [0,0,0,6,0,0,1,0,8]
+    grid[6] = [0,8,6,1,0,0,0,0,0]
+    grid[7] = [0,0,2,3,0,0,0,0,9]
+    grid[8] = [0,0,4,0,9,0,0,0,0]
+    for i in grid:
+#        print(i)
+        for i in range(len(grid)):
+            for s in range(len(grid[i])):
+                if grid[i][s] == 0:
+                    grid[i][s] = '123456789'
+    return grid
+
+
+def newtry4():
+    grid = [['0' for i in range(9)] for i in range(9)]
+
+    grid[0] = [0,0,0,0,5,0,4,0,0]
+    grid[1] = [8,0,0,0,0,3,5,0,0]
+    grid[2] = [0,0,0,0,0,2,8,9,0]
+    grid[3] = [6,0,5,0,0,1,0,0,0]
+    grid[4] = [0,1,0,4,0,9,0,3,0]
+    grid[5] = [0,0,0,6,0,0,1,0,8]
+    grid[6] = [0,8,6,1,0,0,0,0,0]
+    grid[7] = [0,0,2,3,0,0,0,0,9]
+    grid[8] = [0,0,4,0,9,0,0,0,0]
+    for i in grid:
+#        print(i)
+        for i in range(len(grid)):
+            for s in range(len(grid[i])):
+                if grid[i][s] == 0:
+                    grid[i][s] = '123456789'
+    return grid
+
+def newtry5():
+    grid = [['0' for i in range(9)] for i in range(9)]
+
+    grid[0] = [0,0,0,0,0,0,2,9,0]
+    grid[1] = [2,1,0,4,0,3,0,0,0]
+    grid[2] = [3,0,0,0,5,2,0,0,0]
+    grid[3] = [0,0,5,0,0,0,0,6,0]
+    grid[4] = [0,7,0,6,0,8,0,4,0]
+    grid[5] = [0,8,0,0,0,0,7,0,0]
+    grid[6] = [0,0,0,7,6,0,0,0,8]
+    grid[7] = [0,0,0,8,0,1,0,3,4]
+    grid[8] = [0,4,1,0,0,0,0,0,0]
+    for i in grid:
+#        print(i)
+        for i in range(len(grid)):
+            for s in range(len(grid[i])):
+                if grid[i][s] == 0:
+                    grid[i][s] = '123456789'
+    return grid
+
+
+def newtry6():
+    grid = [['0' for i in range(9)] for i in range(9)]
+
+    grid[0] = [0,4,0,0,0,8,0,0,0]
+    grid[1] = [0,0,8,0,0,0,6,7,0]
+    grid[2] = [0,9,0,0,2,0,0,0,0]
+    grid[3] = [9,0,0,8,0,0,4,0,2]
+    grid[4] = [0,2,0,7,0,4,0,3,0]
+    grid[5] = [1,0,4,0,0,3,0,0,5]
+    grid[6] = [0,0,0,0,6,0,0,5,0]
+    grid[7] = [0,5,9,0,0,0,1,0,0]
+    grid[8] = [0,0,0,3,0,0,0,2,0]
+    for i in grid:
+#        print(i)
+        for i in range(len(grid)):
+            for s in range(len(grid[i])):
+                if grid[i][s] == 0:
+                    grid[i][s] = '123456789'
+    return grid
+
+def basictry():
+        grid = [['0' for i in range(9)] for i in range(9)]
+        grid[0][0] = 5
+        grid[0][1] = 3
+        grid[0][4] = 7
+        grid[1][0] = 6
+        grid[1][3] = 1
+        grid[1][4] = 9
+        grid[1][5] = 5
+        grid[2][1] = 9
+        grid[2][2] = 8
+        grid[2][7] = 6
+        grid[3][0] = 8
+        grid[3][4] = 6
+        grid[3][8] = 3
+        grid[4][0] = 4
+        grid[4][3] = 8
+        grid[4][5] = 3
+        grid[4][8] = 1
+        grid[5][0] = 7
+        grid[5][4] = 2
+        grid[5][8] = 6
+        grid[6][1] = 6
+        grid[6][6] = 2
+        grid[6][7] = 8
+        grid[7][3] = 4
+        grid[7][4] = 1
+        grid[7][5] = 9
+        grid[7][8] = 5
+        grid[8][4] = 8
+        grid[8][7] = 7
+        grid[8][8] = 9
+        return grid
+
+def standardize(grid):
+
+#    print(grid)
+    digits = [i for i in range(1,10)]
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] not in digits:
+                grid[x][y] = None
+#    print("stand")
+#    print(grid)
+    return grid
+
+def preprint(line):
+    s = str(line)
+    s = s.replace("None"," ").replace("[","|")
+    s = s.replace("]","|")
+    line = s
+    newline = ""
+    countchar = 0
+    checkchar = ","
+    for char in line:
+        if char == checkchar:
+            countchar +=1
+            if countchar % 3 == 0:
+                newline +='|'
+            else:
+                newline +=' '
+        else:
+            newline+=char
+    return newline
+
+
+def printgrid():
+    print("_"*27)
+    for x in range(len(grid)):
+#        print('x')
+#        print(x)
+        if x > 0 and x % 3 == 0:
+            print("_"*27)
+
+        s = preprint(grid[x])
+
+#        print(len(s))
+        print("{}".format(s))
+
+    print("_"*27)
+
+def main():
+    printgrid()
+
+
+
+if __name__ == "__main__":
+    grid = newtry()
+#    grid = basictry()
+#    grid = newtry2()
+#    grid = newtry3()
+#    grid = newtry4()
+#    grid = newtry5()
+#    grid = newtry6()
+    grid = standardize(grid)
+    main()
