9a85e68e9e695da81326071657a86fbc0841df97
groowe <zivec.jiri@seznam.cz>
2019-08-06 21:01:43 +0200

    safety push

---
+++ surandom.py
@@ -0,0 +1,10 @@
+#from sudoku5 import easy,printgrid,preprint,basicgrid
+from sudoku5 import *
+if __name__ == "__main__":
+    print("asdasd")
+    gridlist = []
+    global grid
+    grid = easy()
+    printgrid()
+    print(grid)
+    main()
