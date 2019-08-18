b2dbb8155053be5c5023c2a3e3f09a614373fae8
groowe <zivec.jiri@seznam.cz>
2019-08-18 12:40:54 +0200

    started graphics

--- sudoku2.py
+++ sudoku2.py
@@ -23,7 +23,7 @@
 # logic (one step) is done by solve()
 # naive way (one step) is done by guessing()
 # combination of it is done by main() # obviously
-
+#########################logic part: ###############
 
 #define grid
 def easy():
@@ -624,7 +624,7 @@ def guessing(poss = None):
         grid = cleanup(grid)
         grid[x][y] = num
 
-def main():
+def main_logic():
     global guessed
     printgrid()
     completed , valid = done()
@@ -714,15 +714,69 @@ def impos3():
 def emptygrid():
     return [["123456789" for i in range(9)] for i in range(9)]
 
+############# end of logic ##########
+########### graphics #############
+import pygame
+
+def printtext(num = '0',x=0,y=0):
+    if type(num) != str:
+        num = str(num)
+    if type(x) != int:
+        x = int(x)
+    if type(y) != int:
+        y = int(y)
+
+    myfont = pygame.font.SysFont('Comic Sans MS',30)
+    textsurface = myfont.render(num,False,white)
+    gameDisplay.blit(textsurface,(x,y))
+
+
+def back(x=0,y=0):
+    gameDisplay.blit(backg,(x,y))
+
+
+def rect(x,y,width,height,color):
+    pygame.draw.rect(gameDisplay,color,[x,y,width,height])
+
+def quitgame():
+    pygame.quit()
+    quit()
+
+
+def button(msg,x,y,w,h,inactive,active):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if ((y+h) > mouse[1] > y) & (( x+ w) > mouse[0] > x):
+        color = active
+    else:
+        color = inactive
+
+    pygame.draw.rect(gameDisplay,color,(x,y,w,h))
+
+    smallText = pygame.font.Font("freesansbold.ttf",20)
+    textSurf , textRect = text_objects(msg,smallText)
+    textRect.center = (( x+ (w/2)),(y+(h/2)) )
+    gameDisplay.blit(textSurf,textRect)
+    if color == active and click[0] == 1:
+        return True
+    return False
+
+
+def text_objects(text,font,color = (0,0,0)):
+    textSurface = font.render(text,True,color)
+    return textSurface,textSurface.get_rect()
+
+
+
 if __name__ == "__main__":
 #    grid = easy()
 #    grid = medium()
 #    grid = hard()
 #    grid = extreme()
-#    grid = hardest()
+    grid = hardest()
 #    grid = impos1()
 #    grid = impos2()
-    grid = impos3()
+#    grid = impos3()
 #    grid = hardest2()
 #    grid = newgrid()
 #    printgrid(False)
@@ -732,7 +786,37 @@ if __name__ == "__main__":
     gridlist = []
 #    solve_by_guess()
     guessed = 0
-    main()
+#    main_logic()
     
+####### graphics : ##############
 
+
+    pygame.init()
+    pygame.display.set_caption('sudoku solver')
+
+    display_width = 1067
+    display_height = 600
+    disp = (display_width,display_height)
+    gameDisplay = pygame.display.set_mode(disp)
+    backgroundpic = "2.png"
+    backg = pygame.image.load(backgroundpic)
+    
+    black = (0,0,0)
+    white = (255,255,255)
+    red = (255,0,0)
+    green = (0,255,0)
+    blue = (0,0,255)
+    gold = (212,175,55)
+
+    while True:
+        back()
+        pygame.display.update()
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        main_logic()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                quitgame()
+#        print(dir(sudoku))
+#        main()
 
